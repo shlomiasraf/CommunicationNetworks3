3483662e8c69e68720b345bbf2eb1c37555a4801
Shlomi Asraf <148898079+shlomiasraf@users.noreply.github.com>
2024-02-28 19:12:31 +0200

    Add files via upload

---
+++ Receiver.c
@@ -0,0 +1,121 @@
+#include <stdio.h> // Standard input-output header
+#include <stdlib.h> // Standard library header
+#include <sys/socket.h> // Socket programming header
+#include <netinet/in.h> // Internet address family header
+#include <string.h> // String manipulation header
+#include <unistd.h> // POSIX operating system API header
+#include <sys/time.h> // Time functions header
+#include <arpa/inet.h>
+#include <bits/time.h>
+#include <time.h>
+
+#define PORT 8080
+#define SERVER_IP "127.0.0.1"
+int runNumber = 1;
+int receiverSocket, senderSocket; // Variables for receiver and sender sockets
+struct sockaddr_in serverAddr; // Server address structure
+char buffer[1024] = {0}; // Buffer for receiving data
+int fileSize = 0; // Variable to store file size
+struct timeval start, end; // Variables to measure time
+double elapsedTime; // Variable to store elapsed time
+double totalBandwidth = 0.0; // Variable to store total bandwidth
+int fileCount = 0; // Variable to count the number of files received
+double totalTime = 0; // the total time from when the files were sent until they arrived
+int createTheSocket()
+{
+    // Create socket for the receiver
+    receiverSocket = socket(AF_INET, SOCK_STREAM, 0); // Create a TCP socket
+    if (receiverSocket == -1) { // Check for socket creation error
+        perror("socket"); // Print error message
+        exit(EXIT_FAILURE); // Exit program with failure status
+    }
+
+    // Setup server address
+    serverAddr.sin_family = AF_INET; // Set address family to IPv4
+    serverAddr.sin_addr.s_addr = inet_addr(SERVER_IP); // Set IP address to localhost
+    serverAddr.sin_port = htons(PORT); // Set port number
+
+    // Bind socket to port
+    if (bind(receiverSocket, (struct sockaddr *) &serverAddr, sizeof(serverAddr)) == -1) { // Bind socket to port
+        perror("bind"); // Print error message
+        exit(EXIT_FAILURE); // Exit program with failure status
+    }
+    printf("Waiting for TCP connection...\n");
+    // Listen for incoming connections
+    if (listen(receiverSocket, 5) == -1) { // Listen for incoming connections
+        perror("listen"); // Print error message
+        exit(EXIT_FAILURE); // Exit program with failure status
+    }
+    // Infinite loop to handle multiple connections
+        // Accept connection from sender
+        socklen_t addrLen = sizeof(serverAddr); // Length of server address structure
+        senderSocket = accept(receiverSocket, (struct sockaddr *) &serverAddr, &addrLen); // Accept connection
+        if (senderSocket == -1)
+        { // Check for accept error
+            perror("accept"); // Print error message
+            exit(EXIT_FAILURE); // Exit program with failure status
+        }
+        printf("Connection accepted from sender.\n"); // Connection accepted message
+}
+void receiveExitSignal(int senderSocket)
+{
+    int exitSignal;
+    recv(senderSocket, &exitSignal, sizeof(int), 0);
+    if (exitSignal == -1)
+    {
+        printf("Received exit signal from server.\nClosing connection.\n");
+        printf("Average time: %f milliseconds\n", totalTime/runNumber);
+        printf("Average bandwidth: %f bytes/millisecond\n", totalBandwidth/runNumber);
+        // Handle the exit signal as needed
+        close(senderSocket);
+        exit(EXIT_SUCCESS);
+    }
+}
+void receiveTheFile(int runNumber)
+{
+    int received = recv(senderSocket, &fileSize, sizeof(int), 0); // Receive file size
+    // Receive file size
+    if (received == -1)
+    { // Check for receive error
+        perror("rec"); // Print error message
+        exit(EXIT_FAILURE); // Exit program with failure status
+    }
+    // Receive file content
+    clock_gettime(CLOCK_MONOTONIC, &start); // Get start time
+    received = recv(senderSocket, buffer, fileSize, 0); // Receive file content
+    if (received == -1)
+    { // Check for receive error
+        perror("recv"); // Print error message
+        exit(EXIT_FAILURE); // Exit program with failure status
+    }
+    else if (received == 0)
+    { // Check if sender closed connection
+        return; // Exit loop
+    }
+    clock_gettime(CLOCK_MONOTONIC, &end); // Get start time
+    // Calculate elapsed time
+    elapsedTime = (end.tv_sec - start.tv_sec) * 1000.0; // Convert seconds to milliseconds
+    elapsedTime += (end.tv_usec - start.tv_usec) / 1000.0; // Convert microseconds to milliseconds
+
+    // Update total bandwidth
+    totalBandwidth += (fileSize / elapsedTime) * 1000.0; // Calculate bandwidth in bytes/ms
+    // Print time and bandwidth
+    printf("runNumber: %d \n", runNumber);
+    printf("Received file in %.2f milliseconds.\n", elapsedTime); // Print elapsed time
+    totalTime += elapsedTime;
+    printf("Bandwidth: %.2f bytes/millisecond\n", fileSize / elapsedTime); // Print bandwidth
+    // Increase file count
+    fileCount++; // Increment file count
+    // Wait for sender response
+    printf("Waiting for sender response...\n"); // Wait message
+    receiveExitSignal(senderSocket);
+}
+int main()
+{
+    createTheSocket();
+    while(1)
+    {
+        receiveTheFile(runNumber);
+        runNumber++;
+    }
+}
---
+++ Sender.c
@@ -0,0 +1,131 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <netinet/tcp.h>
+#include <string.h>
+#include <errno.h>
+#include <signal.h>
+#include <unistd.h>
+
+#define PORT 8080
+#define SERVER_IP "127.0.0.1"
+
+char* fileName = "file.txt";
+char* CC_cubic = "cubic";
+char* CC_reno = "reno";
+int senderSocket;
+int fileSize = 0;
+char *fileContent = NULL;
+
+void readTheFile()
+{
+    printf("Server startup\n");
+
+    printf("Reading file content...\n");
+    FILE *fpointer = fopen(fileName, "r"); // Open the file for reading
+
+    if (fpointer == NULL)
+    { // Check if the file was successfully opened
+        perror("fopen"); // Print an error message if fopen fails
+        exit(EXIT_FAILURE); // Exit the program with failure status
+    }
+
+    fseek(fpointer, 0L, SEEK_END); // Move the file pointer to the end of the file
+    fileSize = ftell(fpointer);    // Get the size of the file
+    fseek(fpointer, 0L, SEEK_SET); // Reset the file pointer to the beginning of the file
+
+    fileContent = (char *)malloc(fileSize * sizeof(char)); // Allocate memory for the file content
+    if (fileContent == NULL)
+    {                      // Check if memory allocation fails
+        perror("malloc");  // Print an error message if malloc fails
+        fclose(fpointer);  // Close the file
+        exit(EXIT_FAILURE); // Exit the program with failure status
+    }
+    fread(fileContent, sizeof(char), fileSize, fpointer); // Read the file content into memory
+    fclose(fpointer); // Close the file
+}
+
+void createTheSocket()
+{
+    // create socket
+    printf("Setting up the socket...\n");
+    struct sockaddr_in serverAddr;
+
+    // Create socket for the sender
+    senderSocket = socket(AF_INET, SOCK_STREAM, 0);
+    // Set congestion control algorithm to Reno
+
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_addr.s_addr = inet_addr(SERVER_IP);
+    serverAddr.sin_port = htons(PORT);
+
+    // Connect to the receiver
+    int bond = connect(senderSocket, (struct sockaddr *)&serverAddr, sizeof(serverAddr));
+    if (bond == -1)
+    {
+        perror("connect");
+        // Handle error
+        exit(EXIT_FAILURE);
+    }
+    else
+    {
+        printf("Connected to receiver.\n");
+    }
+}
+
+void sendTheFile()
+{
+    int sent = send(senderSocket, fileContent, fileSize, 0);
+    if (sent == -1)
+    {
+        perror("send");
+        // Handle error
+        exit(EXIT_FAILURE);
+    }
+    else
+    {
+        printf("File content sent successfully.\n");
+    }
+}
+
+void closeConnection()
+{
+    // exit the program
+    printf("Closing connection...\n");
+    int exitSignal = -1;
+    send(senderSocket, &exitSignal, sizeof(int), 0);
+    // close socket
+    close(senderSocket);
+}
+
+int main()
+{
+    readTheFile();
+    createTheSocket();
+    sendTheFile();
+    printf("do you want to send the file again?\n");
+    char word[4];
+    scanf("%3s", word);
+    while (strcmp(word, "no") != 0 && strcmp(word, "yes") != 0)
+    {
+        printf("please enter yes/no\n");
+        scanf("%3s", word);
+    }
+    while (strcmp(word, "no") != 0)
+    {
+        sendTheFile();
+        printf("do you want to send the file again?\n");
+        scanf("%3s", word);
+        while (strcmp(word, "no") != 0 && strcmp(word, "yes") != 0)
+        {
+            printf("please enter yes/no\n");
+            scanf("%3s", word);
+        }
+    }
+    closeConnection();
+    return 0; // Return success
+}
